46fbeef24884f0e786181ef39945a1712e8a0647
yaotot <yaototmail@gmail.com>
2025-12-25 11:47:34 +0100

    add usagedemo

--- usagedemo.py
+++ usagedemo.py
@@ -189,3 +189,43 @@ agent = create_agent(
 from langchain.agents import AgentState
 from langchain.agents.middleware import AgentMiddleware
 from typing import Any
+
+class CustomState(AgentState):
+    user_perference: dict
+
+class CustomMiddleware(AgentMiddleware):
+    state_schema = CustomState
+    tools = [tool1, tool2]
+
+    def before_model(self, state:CustomState, runtime) -> dict[str, Any] | None:
+        121
+
+agent = create_agent(
+    model,
+    tools=tools,
+    middleware=[CustomMiddleware()]
+)
+
+result = agent.invoke({
+    "messages":[{"role": "user", "content":"I prefer technical explanations"}],
+    "user_perferences": {"style": "technical", "verbosity": "detailed"},
+})
+
+
+######## state_schema##################
+from langchain.agents import AgentState
+
+
+class CustomState(AgentState):
+    user_preferences: dict
+
+agent = create_agent(
+    model,
+    tools=[tool1, tool2],
+    state_schema=CustomState
+)
+# The agent can now track additional state beyond messages
+result = agent.invoke({
+    "messages": [{"role": "user", "content": "I prefer technical explanations"}],
+    "user_preferences": {"style": "technical", "verbosity": "detailed"},
+})
